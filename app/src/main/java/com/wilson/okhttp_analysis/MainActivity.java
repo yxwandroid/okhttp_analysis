409e8c6b6e2fc68ab4357cbf7b87921ff7f09971
wilson <yxw_android@163.com>
2020-05-15 13:47:42 +0800

    update

--- app/src/main/java/com/wilson/okhttp_analysis/MainActivity.java
+++ app/src/main/java/com/wilson/okhttp_analysis/MainActivity.java
@@ -4,42 +4,57 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.LruCache;
+import android.view.View;
 
 
+import com.wilson.okhttp_analysis.utils.ApiUtil;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
-
+   private final String  TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        OkHttpClient client = new OkHttpClient().newBuilder().build();//.addInterceptor();
 
-//        AlertDialog.Builder builder = new AlertDialog.Builder(this);//创建一个Builder对象
-//        builder.setIcon(R.drawable.ic_launcher_foreground);
-//        builder.setTitle("标题");
-//        builder.setMessage("信息");
-//        builder.setPositiveButton("确定",
-//                new DialogInterface.OnClickListener() {
-//                    @Override
-//                    public void onClick(DialogInterface dialog, int which) {
-//                    }
-//                });
-//        AlertDialog alertDialog = builder.create();//创建AlertDialog对象
-//        alertDialog.show();//展示AlertDialo
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //通过Request构造Http请求：需要传入请求地址url
+                Request request = new Request.Builder().url(ApiUtil.APP_UPDATE_URL).build();
+                //创建OkHttpClient对象：目的是通过OkHttpClient初始化Call对象
+                OkHttpClient client = new OkHttpClient();
+                //通过初始化Call对象，来实现网络连接
+                Call call = client.newCall(request);
+                //网络请求回调，回调方法是在非UI线程进行的
+                Callback callback = new Callback() {
+                    @Override
+                    public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                        Log.d(TAG, "onResponse: Response " +response.body().toString());
+                    }
 
+                    @Override
+                    public void onFailure(@NotNull Call call, @NotNull IOException e) {
 
-//        lruCache.get();
-//
-//        new HashMap<>()
+                    }
 
+                };
+                call.enqueue(callback);
+
+            }
+        });
 
 
     }
-
-    private LruCache<String, Bitmap> lruCache;
-
-
 }
---
+++ app/src/main/java/com/wilson/okhttp_analysis/utils/ApiUtil.java
@@ -0,0 +1,8 @@
+package com.wilson.okhttp_analysis.utils;
+
+public class ApiUtil {
+    public static String APP_UPDATE_URL = "https://kefures.huizhidao.com/apk/rom/version.json";
+    public static String APP_UPDATE_CONFIG_SAVE_PATH = "/sdcard/workPhone/version.json";
+    public static String APP_UPDATE_APP_SAVE_PATH = "/sdcard/workPhone/notifyUpdate.apk";
+    public static String APP_ROM_UPDATE_URL = "";
+}
